af32b55e9853a7dd702434841f3764f4e14e874d
Anzel2011 <86427908+Anzel2011@users.noreply.github.com>
2021-12-11 14:53:20 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDhsnu1mk0LvUU-ak_P-tvBAV62JKkXSXU",
+    authDomain: "project92-4e32e.firebaseapp.com",
+    databaseURL: "https://project92-4e32e-default-rtdb.firebaseio.com",
+    projectId: "project92-4e32e",
+    storageBucket: "project92-4e32e.appspot.com",
+    messagingSenderId: "682297063558",
+    appId: "1:682297063558:web:180f05818fd6e984eaf907"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
